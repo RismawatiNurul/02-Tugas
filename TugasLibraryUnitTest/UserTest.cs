22c5dd5358ef3f87503edc8c0047a10943b8e17c
RismawatiNurul <rismawati_nurul@students.amikom.ac.id>
2021-10-27 21:27:17 +0700

    02 Tugas Unit Testing

---
+++ TugasLibraryUnitTest/UserTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using TugasLibrary;
+using NUnit.Framework;
+
+namespace TugasLibraryUnitTest
+{
+    [TestFixture]
+    public class UserTest
+    {
+        private User user;
+
+        [SetUp]
+        public void init()
+        {
+            user = new User();
+        }
+
+        [Test]
+        public void TestValidUser()
+        {
+            var userName = "admin";
+            var password = "admin";
+
+            Assert.IsTrue(user.IsValidUser(userName, password));
+        }
+    }
+}
